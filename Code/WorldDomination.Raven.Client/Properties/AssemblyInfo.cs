34363a2c37a6ef1e4a3dbef3757e9cc7c450bde3
Pure Krome <github@world-domination.com.au>
2013-03-13 15:09:08 +1100

    - Updated NuGet packages and version to 0.2.

--- Code/WorldDomination.Raven.Client/Properties/AssemblyInfo.cs
+++ Code/WorldDomination.Raven.Client/Properties/AssemblyInfo.cs
@@ -1,36 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WorldDomination.Raven.Client")]
 [assembly: AssemblyDescription("Extensions to the Raven.Client library.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("World Domination Technologies, Pty. Ltd.")]
-[assembly: AssemblyProduct("WorldDomination.Raven.Client")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5ffb801f-8298-4291-a2de-930dfb199ae5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
